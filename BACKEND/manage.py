a68e860d2e278b077745c5c481912b34c8d44278
Nick Mol <nickmol96@hotmail.com>
2023-06-15 15:20:59 +0200

    myproject

--- BACKEND/manage.py
+++ BACKEND/manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectapi.settings')
+    settings_module = "projectapi.production" if 'WEBSITE_HOSTNAME' in os.environ else 'projectapi.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+   
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
