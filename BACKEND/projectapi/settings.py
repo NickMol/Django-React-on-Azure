ed748d4f19fd0c4235688557ec460677b16eb69e
Nick Mol <nickmol96@hotmail.com>
2023-06-15 12:18:51 +0200

    added env variable

--- BACKEND/projectapi/settings.py
+++ BACKEND/projectapi/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*qm6hzxe$zv0#vxa24ok(vy5pupg_x2gm==-%(p=rsrb$vxh9d'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
