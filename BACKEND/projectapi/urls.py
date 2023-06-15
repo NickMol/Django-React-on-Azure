6cca8061b31b3b30de3e8970944a1d9d681623ad
Nick Mol <nickmol96@hotmail.com>
2023-06-15 14:57:59 +0200

    added index file

--- BACKEND/projectapi/urls.py
+++ BACKEND/projectapi/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('footballapi/', include('footballapi.urls')),
+    path('', TemplateView.as_view(template_name='backend/index.html')),
 ]
